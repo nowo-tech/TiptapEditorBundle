209eb9fd197eb66051b9e05d3567ce36e6be34d7
Héctor Franco <hectorfranco@nowo.tech>
2026-05-06 11:23:29 +0200

    chore: update CI configuration and add strict types to PHP files

--- demo/symfony7/config/bundles.php
+++ demo/symfony7/config/bundles.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class     => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class               => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class             => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true],
-    Nowo\TiptapEditorBundle\NowoTiptapEditorBundle::class => ['all' => true],
-    Nowo\TwigInspectorBundle\NowoTwigInspectorBundle::class => ['dev' => true, 'test' => true],
+    Nowo\TiptapEditorBundle\NowoTiptapEditorBundle::class     => ['all' => true],
+    Nowo\TwigInspectorBundle\NowoTwigInspectorBundle::class   => ['dev' => true, 'test' => true],
 ];
