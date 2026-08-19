e3aedcc3eb8d6138693852efc8060e975b19bdc7
Héctor Franco <hectorfranco@nowo.tech>
2026-08-19 15:04:30 +0200

    Fix CI: align demo bundles.php for PHP CS Fixer.

--- demo/symfony8/config/bundles.php
+++ demo/symfony8/config/bundles.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-use Nowo\TiptapEditorBundle\NowoTiptapEditorBundle;
 use Nowo\HotReloadBundle\NowoHotReloadBundle;
+use Nowo\TiptapEditorBundle\NowoTiptapEditorBundle;
 use Nowo\TwigInspectorBundle\NowoTwigInspectorBundle;
 use Symfony\Bundle\DebugBundle\DebugBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
@@ -17,7 +17,7 @@ return [
     DebugBundle::class             => ['dev' => true],
     WebProfilerBundle::class       => ['dev' => true],
     NowoTiptapEditorBundle::class  => ['all' => true],
-    NowoHotReloadBundle::class => ['dev' => true, 'test' => true],
+    NowoHotReloadBundle::class     => ['dev' => true, 'test' => true],
     NowoTwigInspectorBundle::class => ['dev' => true, 'test' => true],
     TwigExtraBundle::class         => ['all' => true],
 ];
